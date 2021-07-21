f1a6e9e230e7d1c74e245d3916f1e45614ca77d3
NatashaMelnik <45166335+NatashaMelnik@users.noreply.github.com>
2021-07-21 23:30:57 +0300

    add age calculation func

--- index.js
+++ index.js
@@ -10,28 +10,28 @@
 let birthday1 = new Date(1995, 11, 17);
 let birthday2 = new Date(1995, 5, 23);
 let birthday3 = new Date(1995, 5, 14);
-let birthday4 = new Date(1995, 5, 14);
-
-let employees = [{
-    name: 'Ваня Иванов',
-    birthday: birthday1
-},
-{
-    name: 'Петя Петров',
-    birthday: birthday2
-},
-{
-    name: 'Коля Новогодний',
-    birthday: birthday3
-},
-{
-    name: 'Стас Рождественский',
-    birthday: birthday4
-}
-];
+let birthday4 = new Date(1995, 10, 14);
 
 function main() {
+    let employees = new Map();
+    employees.set('Ваня Иванов', birthday1);
+    employees.set('Петя Петров', birthday2);
+    employees.set('Коля Новогодний', birthday3);
+    employees.set('Стас Рождественский', birthday4);
 
-
+    Display(employees);
 
 }
+
+function Display(map) {
+    console.log(calculateAge(map.get('Коля Новогодний')));
+
+}
+
+function calculateAge(date) {
+    var ageDifMs = Date.now() - date;
+    var ageDate = new Date(ageDifMs);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+}
+
+main();
