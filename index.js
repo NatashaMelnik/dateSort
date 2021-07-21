ad1fc0cae3acb7ea74d58457867c30cc848c6ef7
NatashaMelnik <n.melnik1788@gmail.com>
2021-07-21 22:59:33 +0300

    start consts

---
+++ index.js
@@ -0,0 +1,37 @@
+// console.log(123)
+
+// Декабрь 2020
+// (20) - Ваня Иванов (23 года)
+// (23) - Петя Петров (25 лет)
+// Январь 2021
+// (01) - Коля Новогодний (21 год)
+// (07) - Стас Рождественский (30 лет)
+
+let birthday1 = new Date(1995, 11, 17);
+let birthday2 = new Date(1995, 5, 23);
+let birthday3 = new Date(1995, 5, 14);
+let birthday4 = new Date(1995, 5, 14);
+
+let employees = [{
+    name: 'Ваня Иванов',
+    birthday: birthday1
+},
+{
+    name: 'Петя Петров',
+    birthday: birthday2
+},
+{
+    name: 'Коля Новогодний',
+    birthday: birthday3
+},
+{
+    name: 'Стас Рождественский',
+    birthday: birthday4
+}
+];
+
+function main() {
+
+
+
+}
