fa8ca39d03ed444bd86389d68a25dfefaad576f9
NatashaMelnik <n.melnik1788@gmail.com>
2021-07-23 15:11:31 +0300

    from file reading added

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
+// #!/usr/bin/env node
+
 const GetAllExistMonthes = require('./modules/GetAllExistMonthes');
 const DelSame = require('./modules/DelSame');
 const DispalyEmployees = require('./modules/DispalyEmployees');
+
+const csv = require('csv-parser')
+const fs = require('fs')
+let file = [];
+
+fs.createReadStream('something.csv')
+    .pipe(csv())
+    .on('data', (data) => file.push(data))
+    .on('end', () => {
+        for (let i = 0; i < file.length; i++) {
+            let date = new Date(file[i].birthday);
+            file[i].birthday = date;
+        }
+        main(file)
+    });
+
 
 let birthday1 = new Date('June 17, 1995');
 let birthday2 = new Date('June 12, 1993');
@@ -10,17 +28,18 @@ let birthday5 = new Date('August 13, 2001');
 let birthday6 = new Date('September 14, 2006');
 let birthday7 = new Date('September 11, 1842');
 
-function main() {
+// let inputData = [
+//     { name: 'Ваня Иванов', birthday: birthday1 },
+//     { name: 'Петя Петров', birthday: birthday2 },
+//     { name: 'Коля Новогодний', birthday: birthday3 },
+//     { name: 'Стас Рождественский', birthday: birthday4 },
+//     { name: 'Саша Вакулинский', birthday: birthday5 },
+//     { name: 'Олександр Вакулінский', birthday: birthday6 },
+//     { name: 'Марічка', birthday: birthday7 }
+// ];
 
-    let inputData = [
-        { name: 'Ваня Иванов', birthday: birthday1 },
-        { name: 'Петя Петров', birthday: birthday2 },
-        { name: 'Коля Новогодний', birthday: birthday3 },
-        { name: 'Стас Рождественский', birthday: birthday4 },
-        { name: 'Саша Вакулинский', birthday: birthday5 },
-        { name: 'Олександр Вакулінский', birthday: birthday6 },
-        { name: 'Марічка', birthday: birthday7 },
-    ];
+
+function main(inputData) {
 
     let mapKeys = GetAllExistMonthes(inputData);
 
@@ -46,11 +65,10 @@ function main() {
         }
     }
 
+
+
     let planing = 1; // set monthes to show
     DispalyEmployees(employees, planing);
 }
-
-
-main();
 
 
