64a35bbfb6333b61ddae4b749693f91a95d8d209
NatashaMelnik <n.melnik1788@gmail.com>
2021-07-23 16:15:22 +0300

    final

--- index.js
+++ index.js
@@ -4,42 +4,28 @@ const GetAllExistMonthes = require('./modules/GetAllExistMonthes');
 const DelSame = require('./modules/DelSame');
 const DispalyEmployees = require('./modules/DispalyEmployees');
 
+// let nodePath = process.argv[0];
+// let appPath = process.argv[1];
+let filePath = process.argv[2]; // 'something.csv'
+let index = process.argv[3]; // index
+
 const csv = require('csv-parser')
 const fs = require('fs')
 let file = [];
 
-fs.createReadStream('something.csv')
+fs.createReadStream(filePath)// filePath
     .pipe(csv())
-    .on('data', (data) => file.push(data))
+    .on('data', (data) => { 
+        let day = new Date(data.birthday);
+        file.push({name: data.name, birthday: day}) 
+    })
     .on('end', () => {
-        for (let i = 0; i < file.length; i++) {
-            let date = new Date(file[i].birthday);
-            file[i].birthday = date;
-        }
-        main(file)
+        main(file, index);
     });
 
 
-let birthday1 = new Date('June 17, 1995');
-let birthday2 = new Date('June 12, 1993');
-let birthday3 = new Date('July 16, 1992');
-let birthday4 = new Date('December 19, 1995');
-let birthday5 = new Date('August 13, 2001');
-let birthday6 = new Date('September 14, 2006');
-let birthday7 = new Date('September 11, 1842');
 
-// let inputData = [
-//     { name: 'Ваня Иванов', birthday: birthday1 },
-//     { name: 'Петя Петров', birthday: birthday2 },
-//     { name: 'Коля Новогодний', birthday: birthday3 },
-//     { name: 'Стас Рождественский', birthday: birthday4 },
-//     { name: 'Саша Вакулинский', birthday: birthday5 },
-//     { name: 'Олександр Вакулінский', birthday: birthday6 },
-//     { name: 'Марічка', birthday: birthday7 }
-// ];
-
-
-function main(inputData) {
+function main(inputData, index) {
 
     let mapKeys = GetAllExistMonthes(inputData);
 
@@ -65,10 +51,26 @@ function main(inputData) {
         }
     }
 
-
-
-    let planing = 1; // set monthes to show
-    DispalyEmployees(employees, planing);
+    // let planing = 1; // set monthes to show
+    DispalyEmployees(employees, +index);
 }
 
 
+
+// let birthday1 = new Date('June 17, 1995');
+// let birthday2 = new Date('June 12, 1993');
+// let birthday3 = new Date('July 16, 1992');
+// let birthday4 = new Date('December 19, 1995');
+// let birthday5 = new Date('August 13, 2001');
+// let birthday6 = new Date('September 14, 2006');
+// let birthday7 = new Date('September 11, 1842');
+
+// let inputData = [
+//     { name: 'Ваня Иванов', birthday: birthday1 },
+//     { name: 'Петя Петров', birthday: birthday2 },
+//     { name: 'Коля Новогодний', birthday: birthday3 },
+//     { name: 'Стас Рождественский', birthday: birthday4 },
+//     { name: 'Саша Вакулинский', birthday: birthday5 },
+//     { name: 'Олександр Вакулінский', birthday: birthday6 },
+//     { name: 'Марічка', birthday: birthday7 }
+// ];
--- modules/DispalyEmployees.js
+++ modules/DispalyEmployees.js
@@ -4,14 +4,14 @@ const Print = require('./PrintOutput');
 let monthNamesArr = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Ноябрь', 'Декабрь',];
 let output = ['month'];
 
-function DispalyEmployees(employees, planingIndex) {
+function DispalyEmployees(employees, planingIndex) { // test
     let date = new Date;
     let year = date.getFullYear();
-    let currIndex = +date.getMonth() + 1;
+    let currIndex = +date.getMonth();
     for (let j = currIndex; j <= currIndex + planingIndex; j++) {
         let currMonth;
         for (const [key, value] of employees) {
-            if (key == j) {
+            if (key === j) {
                 let tempMonth = monthNamesArr[key - 1] + ' ' + year;
                 if (tempMonth !== currMonth) {
                     console.log(tempMonth);
